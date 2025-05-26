95f5af9405d68505dd1059cc1ca9b2bb7f726def
Joao Lucas Guedes de Oliveira <iftolucas@gmail.com>
2025-05-26 10:57:59 -0300

    feat: iniciando projeto

---
+++ src/main/java/br/com/jlucasguedes/apisimpalmas/ApisimpalmasApplication.java
@@ -0,0 +1,13 @@
+package br.com.jlucasguedes.apisimpalmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisimpalmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisimpalmasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jlucasguedes/apisimpalmas/ApisimpalmasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jlucasguedes.apisimpalmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApisimpalmasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
